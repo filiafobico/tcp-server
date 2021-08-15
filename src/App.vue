5d757a053869c2657130b80b4bb8116231ebc884
listanegra <vitormac3@hotmail.com>
2021-08-15 17:48:16 -0300

    feat: Vue app entry point

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template></template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App'
+});
+</script>
+
+<style lang="scss">
+@import "tailwindcss/base";
+@import "tailwindcss/components";
+@import "tailwindcss/utilities";
+
+#app {
+  @apply w-screen h-screen;
+}
+</style>
