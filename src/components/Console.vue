df39379be6b95d09c9b4dae5d0bf08dd98480ee3
listanegra <vitormac3@hotmail.com>
2021-08-16 12:41:48 -0300

    feat: Console component implementation

--- src/components/Console.vue
+++ src/components/Console.vue
@@ -1,11 +1,30 @@
 <template>
-    <div class="w-full h-full bg-black rounded"></div>
+    <div class="console overflow-y-auto">
+        <div v-for="message in messages" v-text="message"></div>
+    </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
-    name: 'Console'
+    name: 'Console',
+    setup() {
+        const messages = ref<Array<string>>([]);
+
+        return { messages };
+    },
+    created() {
+        this.$socket.on('data', message => {
+            this.messages.push(message);
+        });
+    }
 });
 </script>
+
+<style lang="scss" scoped>
+div.console {
+    color: green;
+    @apply p-2 w-full h-full bg-black rounded break-all;
+}
+</style>
