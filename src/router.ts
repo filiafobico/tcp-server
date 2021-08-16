1a4850f40e165c0ceadb69972d41486fd0da20d4
listanegra <vitormac3@hotmail.com>
2021-08-16 10:39:37 -0300

    feat: action layout wrapper

--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,20 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
 
 import Menu from '@/views/Menu.vue';
+import Action from '@/views/Action.vue';
 
 const routes: RouteRecordRaw[] = [
     {
         path: '/',
         name: 'menu',
         component: Menu
+    },
+    {
+        path: '/action',
+        component: Action,
+        children: [
+
+        ]
     },
 ];
 
---
+++ src/views/Action.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="flex flex-col h-full space-y-2">
+        <router-view class="h-full"></router-view>
+        <button @click="back">Voltar</button>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'Action',
+    methods: {
+        back() {
+            this.$router.push({ name: 'menu' });
+        }
+    }
+});
+</script>
